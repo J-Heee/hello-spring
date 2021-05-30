437bc61541abdda5b3ed2076ad47f6b2ea28966a
devheee <heee7897@gmail.com>
2021-05-30 12:38:34 +0900

    ADD: API 방식

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,11 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
-    @GetMapping("hello")
+    @GetMapping("hello") //웹에서 /hello로 들어오면 이 메소드 호출해줌
     public String hello(Model model) {
         model.addAttribute("data", "hello!!");
         return "hello";
@@ -18,5 +19,31 @@ public class HelloController {
     public String helloMvc(@RequestParam("name") String name, Model model) { //파라미터를 받음
         model.addAttribute("name", name); //키와 값
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody   //HTTP의 BODY에 데이터를 직접 넣어주겠다는 의미
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;   //객체 반환
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
